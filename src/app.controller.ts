6a9a12abeae4fc7841d4f41af5a7cd30b80cfe70
bobomurod <bobomurod.boboev@yandex.ru>
2021-04-15 00:00:30 +0500

    db connect and other issues

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,10 @@ import { AppService } from './app.service';
 
 @Controller('/api')
 export class AppController {
-  constructor(private appService: AppService) {}
-
-  @Get('/users')
-  getUsers() {
-    return this.appService.getUsers();
-  }
+  // constructor(private appService: AppService) {}
+  //
+  // @Get('/users')
+  // getUsers() {
+  //   return this.appService.getUsers();
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Advanced backend for SPA-websites')
+    .setDescription('Backend Service API documentation')
+    .setVersion('1.0.0')
+    .addTag('Create and Get users from database')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(process.env.APP_PORT, () => {
     console.log(`Server up`);
   });
